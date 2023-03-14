a22e2c1fadab9402969e34631697643d164bc874
ujineeee <yujin11006@naver.com>
2023-03-14 16:09:36 +0900

    modify

--- src/main/java/com/ll/Polynomial.java
+++ src/main/java/com/ll/Polynomial.java
@@ -1,18 +1,54 @@
 package com.ll;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Polynomial {
-    String formula;
-    Polynomial(String _formula){
-        this.formula =  _formula;
-    }
-    public int calc(){
-       int answer = 0;
-        String[] formulaArr = formula.split(" ");
-        if(formulaArr[1].equals("+"))
-            answer = Integer.parseInt(formulaArr[0]) + Integer.parseInt(formulaArr[2]);
-        else if(formulaArr[1].equals("-"))
-            answer = Integer.parseInt(formulaArr[0]) - Integer.parseInt(formulaArr[2]);
-        return answer;
+    public static int run(String exp) {
+        // 단일항이 입력되면 바로 리턴
+        if ( !exp.contains(" ") ) return Integer.parseInt(exp);
+
+        boolean needToMulti = exp.contains(" * ");
+        boolean needToPlus = exp.contains(" + ") || exp.contains(" - ");
+
+        boolean needToCompound = needToMulti && needToPlus;
+
+        if ( needToCompound ) {
+            String[] bits = exp.split(" \\+ ");
+
+            String newExp = Arrays.stream(bits)
+                    .mapToInt(Polynomial::run)
+                    .mapToObj(e -> e + "")
+                    .collect(Collectors.joining(" + "));
+
+            return run(newExp);
+        }
+        else if ( needToPlus ) {
+            exp = exp.replaceAll("- ", "+ -");
+
+            String[] bits = exp.split(" \\+ ");
+
+            int sum = 0;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum += Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+        else if ( needToMulti ) {
+            String[] bits = exp.split(" \\* ");
+
+            int sum = 1;
+
+            for (int i = 0; i < bits.length; i++) {
+                sum *= Integer.parseInt(bits[i]);
+            }
+
+            return sum;
+        }
+
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 }
 
